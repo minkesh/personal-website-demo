e4328bfd4df605cc66acaed096c7ccf99f8baad6
L4o <l4o@Minkeshs-Macbook.local>
2022-02-21 00:02:33 +0530

    fixed menu link path and added deploy workflow

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,21 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import './menu.css';
 
 const Menu = () => {
+    const { pathname } = useLocation();
     return (
         <Navbar bg="light" expand="lg">
             <Container className='nav-container'>
-                <Navbar.Brand as={Link} to="/">My Website</Navbar.Brand>
+                <Navbar.Brand as={Link} to={`${pathname}/`}>My Website</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/Home">
+                        <Nav.Link as={Link} to={`${pathname}/home`}>
                             Home
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/blog">
+                        <Nav.Link as={Link} to={`${pathname}/blog`}>
                             Blog
                         </Nav.Link>
                     </Nav>
