706d678348f18d73c61f29a89bc1c577d0c50d3a
L4o <l4o@Minkeshs-Macbook.local>
2022-02-24 07:44:17 +0530

    fixed routig

--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,19 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import './menu.css';
 
 const Menu = () => {
-    const { pathname } = useLocation();
     return (
         <Navbar bg="light" expand="lg">
             <Container className='nav-container'>
-                <Navbar.Brand as={Link} to={`${pathname}/`}>My Website</Navbar.Brand>
+                <Navbar.Brand as={Link} to={'personal-website-demo/'}>My Website</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to={`${pathname}/home`}>
+                        <Nav.Link as={Link} to={'personal-website-demo/home'}>
                             Home
                         </Nav.Link>
-                        <Nav.Link as={Link} to={`${pathname}/blog`}>
+                        <Nav.Link as={Link} to={'personal-website-demo/blog'}>
                             Blog
                         </Nav.Link>
                     </Nav>
